1362906f308ca3da88b101fc9ac31836bd5370b8
Andrei Todor <bvandrei94@gmail.com>
2019-09-10 22:58:01 +0200

    change server address

--- server/server.rb
+++ server/server.rb
@@ -8,7 +8,7 @@ port = ENV["PORT"]
 logfile = File.open("logs.txt", "w")
 logfile.puts "using port #{port}"
 
-server = TCPServer.new('localhost', port)
+server = TCPServer.new('0.0.0.0', port)
 
 def handle_connection(client, logfile)
   request = client.readpartial(2048)
