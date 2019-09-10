28895e2127f3fb12487b42a00fd9dc09c33eeda5
Andrei Todor <bvandrei94@gmail.com>
2019-09-10 22:55:13 +0200

    add logfile to store logs

--- server/server.rb
+++ server/server.rb
@@ -5,23 +5,29 @@ include RequestParser
 include PrepareResponse
 
 port = ENV["PORT"]
-puts "using port #{port}"
-server = TCPServer.new('0.0.0.0', port)
+logfile = File.open("logs.txt", "w")
+logfile.puts "using port #{port}"
 
-def handle_connection(client)
+server = TCPServer.new('localhost', port)
+
+def handle_connection(client, logfile)
   request = client.readpartial(2048)
   request = RequestParser.parse(request)
 
-  puts request
-  puts
+  logfile.puts
+  logfile.puts request
+  logfile.puts
 
   response = PrepareResponse.prepare(request)
+  logfile.puts "############################################################"
   response.send(client)
 end
 
 loop do
   Thread.fork(server.accept) do |client| 
-    handle_connection(client)
+    handle_connection(client, logfile)
     client.close
   end
 end
+
+logfile.close
